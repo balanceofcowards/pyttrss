d3e5d6493b49a099b8ce2914a6465db39df40732
Andreas Fischer <cyberfrag@gmx.net>
2017-08-07 20:52:53 +0200

    More elaborate console client

--- feedline.py
+++ feedline.py
@@ -1,15 +1,34 @@
 import readchar
 import subprocess
+import argparse
+import getpass
+import json
+import os.path
 from ttrss import TinyTinyRSS
 
-CONN = {
-        "url": None,
-        "user": None,
-        "password": None
-        }
-
 if __name__ == "__main__":
-    with TinyTinyRSS(CONN) as ttrss:
+    conn = {}
+    if os.path.isfile('pyttrss.cfg'):
+        with open('pyttrss.cfg', 'r') as cfgfile:
+            conn = json.load(cfgfile)
+
+    parser = argparse.ArgumentParser(description='Show a list of recent articles.')
+    parser.add_argument('-u', '--user', help='Username')
+    parser.add_argument('-p', '--password', help='Password')
+    parser.add_argument('-U', '--url', help='Server URL')
+    args = parser.parse_args()
+    for key, value in vars(args).iteritems():
+        if value:
+            conn[key] = value
+
+    if not conn['user']:
+        conn['user'] = raw_input("Enter username: ")
+    if not conn['password']:
+        conn['password'] = getpass.getpass()
+    if not conn['url']:
+        conn['url'] = raw_input("Enter server URL: ")
+
+    with TinyTinyRSS(conn) as ttrss:
         print "Unread articles:", ttrss.getUnread()
         for article in ttrss.newarticles():
             outstr = u"{:>20} | {}".format(article['feed_title'][:20], article['title'])
