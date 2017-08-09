e5a2cd6d3888a42f8d71a7475e2aa2d588ac23e7
Andreas Fischer <andreas.fischer@kau.se>
2017-08-09 16:10:51 +0200

    Improvements to the feedline script

--- feedline.py
+++ feedline.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import readchar
+"""
+Rapidly display fresh headlines from a TinyTinyRSS instance on the command line.
+
+(c) 2017 Andreas Fischer <_@ndreas.de>
+"""
 import subprocess
 import argparse
 import getpass
 import json
 import os.path
+import readchar
 from ttrss import TinyTinyRSS
 
-if __name__ == "__main__":
+def get_conn():
+    """
+    Get connection details either from a config file, the commandline, or via user input.
+    """
     conn = {}
     if os.path.isfile('pyttrss.cfg'):
         with open('pyttrss.cfg', 'r') as cfgfile:
             conn = json.load(cfgfile)
 
-    parser = argparse.ArgumentParser(description='Show a list of recent articles.')
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-u', '--user', help='Username')
     parser.add_argument('-p', '--password', help='Password')
     parser.add_argument('-U', '--url', help='Server URL')
@@ -29,8 +38,10 @@ if __name__ == "__main__":
         conn['password'] = getpass.getpass()
     if not conn['url']:
         conn['url'] = raw_input("Enter server URL: ")
+    return conn
 
-    with TinyTinyRSS(conn) as ttrss:
+if __name__ == "__main__":
+    with TinyTinyRSS(get_conn()) as ttrss:
         print "Unread articles:", ttrss.getUnread()
         for article in ttrss.getHeadlines(feed_id=-3):
             outstr = u"{:>20} | {}".format(article['feed_title'][:20], article['title'])
